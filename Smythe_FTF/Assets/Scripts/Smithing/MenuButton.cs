dad08d3b57050ecd44b6681b8d8ead06459577b1
pyxtess <pyxtess@users.noreply.github.com>
2020-06-15 19:20:07 -0400

    Added a simple menu UI
    +Added simple UI
    -Doesn't do anything

---
+++ Smythe_FTF/Assets/Scripts/Smithing/MenuButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] MenuController menuController;
+    [SerializeField] Animator anim;
+    //[SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuController.index == thisIndex)
+        {
+            anim.SetBool("selected", true);
+            if (Input.GetAxis("Submit") == 1)
+                anim.SetBool("pressed", true);
+            else if (anim.GetBool("pressed"))
+                anim.SetBool("pressed", false);
+        }
+        else
+            anim.SetBool("selected", false);
+    }
+}
